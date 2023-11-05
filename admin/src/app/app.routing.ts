e1fc05118b0efceedd1f38bdba9f1782b682414c
Brayan Emanuel Sanchez Ramirez <brayan.sanchez.contacto@gmail.com>
2023-11-04 18:38:21 -0600

    proteccion de componentes con guard panel administrador

--- admin/src/app/app.routing.ts
+++ admin/src/app/app.routing.ts
@@ -2,11 +2,18 @@ import { ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { LoginComponent } from './components/login/login.component';
+import { AdminGuard } from './guards/admin.guard';
 
 const appRoute: Routes = [
   {
     path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full',
+  },
+  {
+    path: 'inicio',
     component: InicioComponent,
+    canActivate: [AdminGuard],
   },
   {
     path: 'login',
