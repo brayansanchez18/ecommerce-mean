3d808722e22c40733e056de12b48ee81c541a4d1
Brayan Emanuel Sanchez Ramirez <brayan.sanchez.contacto@gmail.com>
2024-01-12 23:33:42 -0600

    configurar nouislider para rango de precios

--- shop/src/app/components/productos/index-productos/index-productos.component.ts
+++ shop/src/app/components/productos/index-productos/index-productos.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var noUiSlider: any;
+declare var $: any;
 
 @Component({
   selector: 'app-index-productos',
   templateUrl: './index-productos.component.html',
-  styleUrls: ['./index-productos.component.css']
+  styleUrls: ['./index-productos.component.css'],
 })
-export class IndexProductosComponent {
+export class IndexProductosComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {
+    var slider: any = document.getElementById('slider');
+    noUiSlider.create(slider, {
+      start: [0, 1000],
+      connect: true,
+      decimals: false,
+      range: {
+        min: 0,
+        max: 1000,
+      },
+      tooltips: [true, true],
+      pips: {
+        mode: 'count',
+        values: 5,
+      },
+    });
+
+    slider.noUiSlider.on('update', function (values: any) {
+      $('.cs-range-slider-value-min').val(values[0]);
+      $('.cs-range-slider-value-max').val(values[1]);
+    });
+    $('.noUi-tooltip').css('font-size', '11px');
+  }
 }
